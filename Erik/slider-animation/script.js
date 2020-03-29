154edcf510a9623a2aba9e40683ee78a7894fff9
lundow <eriklundow@gmail.com>
2020-03-29 22:26:16 +0200

    Slider animation

---
+++ Erik/slider-animation/script.js
@@ -0,0 +1,36 @@
+var links = document.querySelectorAll(".itemLinks");
+var wrapper = document.querySelector("#wrapper");
+
+var activeLink = 0;
+
+for (var i = 0; i < links.length; i++) {
+  var link = links[i];
+  link.addEventListener("click", setClickedItem, false);
+  link.itemID = i;
+}
+
+links[activeLink].classList.add("active");
+
+function setClickedItem(e) {
+  removeActiveLinks();
+
+  var clickedLink = e.target;
+  activeLink = clickedLink.itemID;
+
+  changePosition(clickedLink);
+}
+
+function removeActiveLinks() {
+  for (var i = 0; i < links.length; i++) {
+    links[i].classList.remove("active");
+  }
+}
+
+function changePosition(link) {
+  var position = link.getAttribute("data-pos");
+
+  var translateValue = "translate3d(" + position + ", 0px, 0)";
+  wrapper.style.transform = translateValue;
+
+  link.classList.add("active");
+}
