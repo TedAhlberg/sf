d5719474c44284d9f07dc3d14aad1ebae913c90e
Ted Ahlberg <ted.ahlberg95@gmail.com>
2020-05-19 11:11:59 +0200

    page2 navigation done

--- Ted/page2/script.js
+++ Ted/page2/script.js
@@ -1,22 +1,23 @@
+// Search input
+const inputFocus = () => {
+  $("input").css("width", "84%")
+  $(".search-border").addClass("search-border-focus")
+  $(".search-icon").addClass("search-icon-focus")
+}
+
+const inputFocusout = () => {
+  $("input").css("width", "0%")
+  $(".search-border").removeClass("search-border-focus")
+  $(".search-icon").removeClass("search-icon-focus")
+}
+
 $(() => {
   $(".search-icon").click(() => {
     $("input").focus()
   })
 
-  const inputFocus = () => {
-    $("input").css("width", "84%")
-    $(".search-border").addClass("search-border-focus")
-    $(".search-icon").addClass("search-icon-focus")
-  }
-
-  const inputFocusout = () => {
-    $("input").css("width", "0%")
-      $(".search-border").removeClass("search-border-focus")
-      $(".search-icon").removeClass("search-icon-focus")
-  }
-
   var inputValue = $("input").val()
-  if(inputValue.length > 0)
+  if (inputValue.length > 0)
     inputFocus()
 
   $("input").on("focus", () => {
@@ -28,5 +29,33 @@ $(() => {
     let value = $("input").val()
     if (value.length === 0)
       inputFocusout()
+  })
+
+})
+
+//Navigation
+const getItemElements = (event) => {
+  let elements = []
+  elements[0] = $(event.target.childNodes[1])
+  elements[1] = $(event.target.childNodes[3])
+  elements[2] = $(event.target.childNodes[5])
+  return elements
+}
+
+$(() => {
+  //mouseover
+  $(".nav-item").on("mouseover", (event) => {
+    let itemElements = getItemElements(event)
+    itemElements[0].addClass("nav-item-mouseover")
+    itemElements[1].addClass("nav-item-mouseover")
+    itemElements[2].addClass("nav-arrow-mouseover")
+  })
+
+  //mouseleave
+  $(".nav-item").on("mouseleave", (event) => {
+    let itemElements = getItemElements(event)
+    itemElements[0].removeClass("nav-item-mouseover")
+    itemElements[1].removeClass("nav-item-mouseover")
+    itemElements[2].removeClass("nav-arrow-mouseover")
   })
 })
