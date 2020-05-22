17d36f49a3bbc71d622fc40c67f8b5c4c426f0d3
Ted Ahlberg <ted.ahlberg95@gmail.com>
2020-05-22 23:51:17 +0200

    page 3 complete

--- Ted/page3/res/info.js
+++ Ted/page3/res/info.js
@@ -2,6 +2,7 @@ const info = {
   albums: [
     {
       title: "UNRENDERED",
+      cover: "cover1.png",
       tracklist: [
         { title: "Importing", time: "0:57" },
         { title: "CtrlAltDelete", time: "2:58" },
@@ -21,6 +22,101 @@ const info = {
         { title: "TheGrandestNothing", time: "2:23" },
         { title: "ContinueWithoutSaving", time: "3:21" },
         { title: "Exporting", time: "3:19" }
+      ]
+    },
+    {
+      title: "NoRedeemingQualities",
+      cover: "cover2.png",
+      tracklist: [
+        { title: "Oxygen", time: "2:22" },
+        { title: "TheDeadMansTrunk", time: "1:56" },
+        { title: "SeanPaulWasNeverThereToGiveMeTheLight", time: "4:21" },
+        { title: "Decomposing", time: "1:08" },
+        { title: "WasteOfSpace", time: "2:02" },
+        { title: "WhatAGreatDayToStayIndoors", time: "3:02" },
+        { title: "l2l", time: "1:23" },
+        { title: "TimTheToolmanTaylor", time: "2:28" },
+        { title: "Roaming", time: "2:28" },
+        { title: "AvrilWasRight,EverythingIsSoComplicated", time: "1:39" },
+        { title: "ExoticBambooFlooring", time: "2:06" },
+        { title: "YouMadeYourBed,NowLieInIt", time: "1:44" },
+        { title: "BridgetO'Malley", time: "1:16" },
+        { title: "Drown", time: "5:00" }
+      ]
+    },
+    {
+      title: "disgrace",
+      cover: "cover3.png",
+      tracklist: [
+        { title: "DISGRACE", time: "0:45" },
+        { title: "Branches", time: "2:06" },
+        { title: "PleaseHangUpAndTryAgain", time: "2:24" },
+        { title: "04 EyeForAnEye", time: "1:29" },
+        { title: "GoodAfternoonGoodEveningAndGoodNight", time: "1:24" },
+        { title: "DoNotDisturb", time: "2:03" },
+        { title: "AsTheAncienthawaiiansUsedToSay", time: "3:12" },
+        { title: "BedfordFalls", time: "2:58" },
+        { title: "PileOfFlesh", time: "2:41" },
+        { title: "BiggestLetdown", time: "2:28" },
+        { title: "TheRoadLessTraveled", time: "2:30" },
+        { title: "SkeletonMan", time: "2:46" },
+      ]
+    },
+    {
+      title: "Skinny",
+      cover: "cover4.png",
+      tracklist: [
+        { title: "Skinny", time: "1:09" },
+        { title: "WhitePunkOnDope", time: "2:26" },
+        { title: "HollisterJeans", time: "3:03" },
+        { title: "HairHangDown", time: "1:36" },
+        { title: "USB", time: "1:52" },
+        { title: "Protein", time: "1:35" },
+        { title: "IfYoureGoingToSpew,SpewIntoThis", time: "1:22" },
+        { title: "LifeAsAHouse", time: "0:51" },
+        { title: "TheNoiseInsideMyHead", time: "2:41" },
+        { title: "HighVisibilityRainCoat", time: "2:58" },
+        { title: "ShrimpPizza", time: "2:51" },
+        { title: "DeadInDesignerClothes", time: "2:28" },
+        { title: "CanbrooksEstates", time: "1:55" },
+        { title: "EmilioEstevez,TheMightyDuckMan", time: "1:30" },
+        { title: "Bubbles", time: "2:14" },
+        { title: "WhereTheTreesMeetTheFreeway", time: "2:55" },
+        { title: "SketchersBackToSchoolSale", time: "1:32" }
+      ]
+    },
+    {
+      title: "Powder",
+      cover: "cover5.png",
+      tracklist: [
+        { title: "Powder", time: "0:34" },
+        { title: "TreeOfLife", time: "1:36" },
+        { title: "HereGoesNothing", time: "2:08" },
+        { title: "TheHealingFields", time: "4:01" },
+        { title: "ItAllComesTogetherInTheFinalAct", time: "2:48" },
+        { title: "HeySorry,IWasInTheShower", time: "2:00" },
+        { title: "HelloFriend ThatStuffWillKillYa", time: "2:00" },
+        { title: "ICanSeeMyHouseFromHere", time: "1:54" },
+        { title: "Kale", time: "2:51" },
+        { title: "ModestGoals", time: "1:44" },
+        { title: "D-19", time: "1:52" },
+        { title: "Chlorella", time: "1:43" },
+        { title: "HimalayanSalt", time: "2:19" },
+        { title: "TillTheVulturesCome", time: "1:41" },
+        { title: "ThisPartyReallyDied", time: "1:43" },
+        { title: "JazzDads,TakeOne", time: "2:27" },
+        { title: "ItsTheLittleThings", time: "1:24" },
+        { title: "Biophony", time: "2:33" },
+        { title: "TheMaritimeEnglandSuite", time: "1:26" },
+        { title: "IThoughtYouLookedFarmiliar", time: "1:44" },
+        { title: "UponTheDyingGrass", time: "2:44" },
+        { title: "Neoprene", time: "3:04" },
+        { title: "TakeItFromMe", time: "0:53" },
+        { title: "ComeListenToThisShit,IFoundMyUnclesOldWalkMan", time: "1:50" },
+        { title: "TryToStayInTheFarmThisTime", time: "3:50" },
+        { title: "TheAnnextionOfPuertoRico", time: "1:19" },
+        { title: "JustAnotherDayAtTheOffice", time: "2:12" },
+        { title: "HowAboutThat", time: "0:48" }
       ]
     }
   ]
--- Ted/page3/script.js
+++ Ted/page3/script.js
@@ -1,17 +1,10 @@
-// Text function
-const showText = (i) => {
-  let str = "CH0" + i
-  $("#text").empty().text(str)
-  $("#text").removeClass("text-off")
-  setTimeout(() => {
-    $("#text").addClass("text-off")
-  }, 4500)
-}
+var inputLock = true
+var albumIndex = 0
 
 // Start timer
 setTimeout(() => {
   $("#crt").removeClass("crt-off")
-  showText(1)
+  showText(albumIndex)
   $("#cover").removeClass("cover-off")
   $("#cover").addClass("cover-start")
 }, 2000)
@@ -24,28 +17,85 @@ setTimeout(() => {
   displayAlbum(0)
 }, 4500)
 
-// Arrow hovers
-$(() => {
-  $("#arrow-left").on("mouseover", () => {
-    $("#arrow-left").addClass("arrow-hover")
-  })
-  $("#arrow-left").on("mouseout", () => {
-    $("#arrow-left").removeClass("arrow-hover")
-  })
+// Input lock
+setTimeout(() => {
+  inputLock = false
+  $("#arrow-left-wrapper").css("cursor", "pointer")
+  $("#arrow-right-wrapper").css("cursor", "pointer")
+  $("#cover").removeClass("cover-start")
+}, 6000)
 
-  $("#arrow-right").on("mouseover", () => {
-    $("#arrow-right").addClass("arrow-hover")
+// Shift funcitons
+const shiftLeft = () => {
+  inputLock = true
+  if (albumIndex === 0) albumIndex = 4
+  else albumIndex--
+
+  $("#cover").css("background-image",
+    "url(\"./res/" + info.albums[albumIndex].cover + "\")"
+  ).addClass("cover-switch")
+  showText(albumIndex)
+  hideAlbumElements()
+  setTimeout(() => {
+    displayAlbum(albumIndex)
+  }, 1000)
+}
+
+const shiftRight = () => {
+  inputLock = true
+  if (albumIndex === 4) albumIndex = 0
+  else albumIndex++
+
+  $("#cover").css("background-image",
+    "url(\"./res/" + info.albums[albumIndex].cover + "\")"
+  ).addClass("cover-switch")
+  showText(albumIndex)
+  hideAlbumElements()
+  setTimeout(() => {
+    displayAlbum(albumIndex)
+  }, 1750)
+}
+
+$(() => {
+  $("#arrow-left").on("click", () => {
+    if (!inputLock)
+      shiftLeft()
   })
-  $("#arrow-right").on("mouseout", () => {
-    $("#arrow-right").removeClass("arrow-hover")
+  $("#arrow-right").on("click", () => {
+    if (!inputLock)
+      shiftRight()
   })
 })
 
+// Hide functions
+const hideAlbumElements = () => {
+  $(".album-header").addClass("hide-element")
+  $(".album-tracks").addClass("hide-element")
+  $("#footer").addClass("hide-element")
+}
+
+// Text function
+const showText = (i) => {
+  let str = "CH0" + (i + 1)
+  $("#text").empty().text(str)
+  $("#text").removeClass("text-off")
+  setTimeout(() => {
+    $("#text").addClass("text-off")
+  }, 4500)
+}
+
+// Display Album & Footer
 const displayAlbum = (i) => {
   $(".album-header").text(info.albums[i].title)
+  $(".album-header").removeClass("hide-element")
   $(".album-header").addClass("album-header-enter")
+
+  $(".album-tracks").empty()
+  $(".album-tracks").removeClass("hide-element")
+
   let j = 0;
   let length = info.albums[i].tracklist.length
+
   let loop = () => {
     let title = info.albums[i].tracklist[j].title
     let time = info.albums[i].tracklist[j].time
@@ -53,12 +103,12 @@ const displayAlbum = (i) => {
     setTimeout(() => {
       let $track = $(
         "<div class=\"track\">" +
-          "<span class=\"track-title\">"
-           + title +
-          "</span>" +
-          "<span class=\"track-time\">"
-           + time +
-          "</span>" +
+        "<span class=\"track-title\">"
+        + title +
+        "</span>" +
+        "<span class=\"track-time\">"
+        + time +
+        "</span>" +
         "</div>"
       )
       $track.appendTo(".album-tracks")
@@ -66,7 +116,13 @@ const displayAlbum = (i) => {
       j++
       if (j < length)
         loop()
-    }, 200)
+      else {
+        $(".cover").removeClass("cover-switch")
+        $("#footer").removeClass("hide-element")
+        $("#footer").addClass("footer-enter")
+        inputLock = false
+      }
+    }, 100)
   }
   loop()
 }
